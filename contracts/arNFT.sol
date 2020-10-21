3836bb1a70ae785a1ca346eaae8ba675be68ab60
RobertMCForster <rforster510@yahoo.com>
2020-10-21 01:54:03 -0700

    Added current time to BuyCover event

--- contracts/arNFT.sol
+++ contracts/arNFT.sol
@@ -94,7 +94,7 @@ contract arNFT is
         bytes4 currency,
         uint256 coverAmount,
         uint256 coverPrice,
-        uint256 coverPriceNXM,
+        uint256 startTime,
         uint16 coverPeriod
     );
 
@@ -152,7 +152,7 @@ contract arNFT is
         _mint(msg.sender, coverId);
         
         emit BuyCover(coverId, msg.sender, _coveredContractAddress, _coverCurrency, _coverDetails[0], _coverDetails[1], 
-                      _coverDetails[2], _coverPeriod);
+                      block.timestamp, _coverPeriod);
     }
     
     /**
